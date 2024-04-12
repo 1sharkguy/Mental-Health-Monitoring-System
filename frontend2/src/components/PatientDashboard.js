7ce630e7247042df2d1bc952bc857f33d7dc7460
Divyansh Garg <107041307+1sharkguy@users.noreply.github.com>
2024-04-13 02:07:14 +0530

    Update PatientDashboard.js

--- frontend2/src/components/PatientDashboard.js
+++ frontend2/src/components/PatientDashboard.js
@@ -99,7 +99,7 @@ function PatientDashboard() {
     const handleDeletePatient = async () => {
         try {
             if (selectedPatient) {
-                const response = await axios.get(process.env.REACT_APP_URL_DELETEP.replace('{patient_id}', selectedPatient.id));
+                const response = await axios.get(`${process.env.REACT_APP_URL_DELETEP}/${selectedPatient.id}`);
 
                 if (response.status === 200) {
                     console.log('Patient and associated analysis deleted successfully');
